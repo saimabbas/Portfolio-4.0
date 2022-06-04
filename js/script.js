338ef87accf326898921711b464029ea1688f6ef
Saim Abbas <saimabbas220022@gmail.com>
2022-06-04 11:54:46 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log("abc");
+var swiper = new Swiper(".testimonials-swiper", {
+  pagination: {
+    el: ".testimonials-pagination",
+  },
+  navigation: {
+    nextEl: ".testimoonial-right",
+    prevEl: ".testimoonial-left",
+  },
+  autoHeight: false,
+  speed: 1000,
+  spaceBetween: 50,
+  centeredSlides: true,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+});
