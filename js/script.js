983bfd3e681e5c29b2e3a735107565487db8df7a
Saim Abbas <saimabbas220022@gmail.com>
2022-06-05 00:19:54 +0500

    About

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ var swiper = new Swiper(".testimonials-swiper", {
   spaceBetween: 100,
   centeredSlides: true,
   loop: true,
+  slidesPerView: 1,
   effect: "coverflow",
   coverflowEffect: {
     rotate: 50,
@@ -20,4 +21,40 @@ var swiper = new Swiper(".testimonials-swiper", {
     modifier: 1,
     slideShadows: false,
   },
+});
+
+const degreeToRadian = (angle) => {
+  return angle * (Math.PI / 180);
+};
+
+const radius = 150;
+const diameter = radius * 2;
+
+const circle = document.querySelector("#circular-text");
+circle.style.width = `${diameter}px`;
+circle.style.height = `${diameter}px`;
+
+const text = circle.innerText;
+const characters = text.split("");
+circle.innerText = null;
+
+const startAngle = -90;
+const endAngle = 270;
+const angleRange = endAngle - startAngle;
+
+const deltaAngle = angleRange / characters.length;
+let currentAngle = startAngle;
+
+characters.forEach((char, index) => {
+  const charElement = document.createElement("span");
+  charElement.innerText = char;
+  const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
+  const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+
+  const transform = `translate(${xPos}px, ${yPos}px)`;
+  const rotate = `rotate(${index * deltaAngle}deg)`;
+  charElement.style.transform = `${transform} ${rotate}`;
+
+  currentAngle += deltaAngle;
+  circle.appendChild(charElement);
 });
